4cd254e0420f04fe1c1cf4c2fd2202b6713036cb
eric <wangyujie01@hexindai.com>
2020-03-23 10:12:51 +0800

    两两交换节点

---
+++ src/main/java/com/eureka/test/algorithms/hard/MergeNodeList.java
@@ -0,0 +1,135 @@
+package com.eureka.test.algorithms.hard;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * <p>合并链表</p>
+ * https://leetcode-cn.com/problems/merge-k-sorted-lists/
+ *
+ * @Author : Eric
+ * @Date: 2020-03-22 14:47
+ */
+public class MergeNodeList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 分而治之
+     *
+     * @param lists
+     * @return
+     */
+    public static ListNode mergeKLists(ListNode[] lists) {
+        int n = lists.length;
+        if (n == 0) {
+            return null;
+        }
+        while (n > 1) {
+            int i;
+            for (i = 0; i < n / 2; i++) {
+                lists[i] = mergeNode(lists[2 * i], lists[2 * i + 1]);
+            }
+
+            if (n % 2 != 0) {
+                lists[i] = lists[n - 1];
+                n = n + 1;
+            }
+
+            n = n / 2;
+        }
+
+        return lists[0];
+    }
+
+
+    public static ListNode mergeNode(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while (l1 != null || l2 != null) {
+
+            if (l1 == null) {
+                cur.next = l2;
+                break;
+            } else if (l2 == null) {
+                cur.next = l1;
+                break;
+            }
+
+
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+
+        return dummy.next;
+    }
+
+    /**
+     * 需要一种辅助数据结构-堆
+     *
+     * @param lists
+     * @return
+     */
+    public static ListNode mergeKListsTwo(ListNode[] lists) {
+        int n = lists.length;
+        if (n == 0) {
+            return null;
+        }
+        PriorityQueue<ListNode> queue = new PriorityQueue(new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val - o2.val;
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                return false;
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            ListNode head = lists[i];
+            if (head != null) {
+                queue.add(head);
+            }
+        }
+
+
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while (!queue.isEmpty()) {
+            ListNode node = queue.poll();
+            cur.next = node;
+            cur = cur.next;
+            if (node != null && node.next != null) {
+                queue.add(node.next);
+            }
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(5);
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(2);
+        l2.next.next = new ListNode(4);
+        ListNode[] ls = {l1, l2};
+        System.out.println(mergeKListsTwo(ls));
+    }
+}
---
+++ src/main/java/com/eureka/test/algorithms/normal/SwapNodePairs.java
@@ -0,0 +1,70 @@
+package com.eureka.test.algorithms.normal;
+
+/**
+ * <p>两两交换节点</p>
+ *
+ * @Author : Eric
+ * @Date: 2020-03-22 16:02
+ */
+public class SwapNodePairs {
+
+    static class ListNode {
+        int val;
+
+        ListNode next;
+
+        public ListNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 非递归解法
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode tmp = dummy;
+        while (tmp.next != null && tmp.next.next != null) {
+
+            ListNode start = tmp.next;
+            ListNode next = tmp.next.next;
+
+            tmp.next = next;
+            start.next = next.next;
+            next.next = start;
+            tmp = start;
+        }
+        return dummy.next;
+    }
+
+    /**
+     * 递归法
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode swapPairsTwo(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairsTwo(next.next);
+        next.next = head;
+        return next;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode n = new ListNode(1);
+        n.next = new ListNode(4);
+        n.next.next = new ListNode(2);
+        n.next.next.next = new ListNode(8);
+        n.next.next.next.next = new ListNode(6);
+        System.out.println(swapPairsTwo(n));
+
+    }
+}
